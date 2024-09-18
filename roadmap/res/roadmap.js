93b39a6aad0da91379dffb4734472eb0815d4f97
MrJino <for1self@gmail.com>
2024-09-18 13:05:44 +0900

    main

---
+++ roadmap/res/roadmap.js
@@ -0,0 +1,16 @@
+function focusTopic(topic) {
+  const focusElements = document.getElementsByClassName('article_circle');
+  Array.from(focusElements).forEach((element) => {
+    const elementName = element.getAttribute('name');
+    if (elementName === topic) {
+      element.classList.add('focused');
+    } else {
+      element.classList.add('pointer');
+      element.addEventListener('click', function () {
+        const currentUrl = new URL(window.location.href);
+        currentUrl.searchParams.set('topic', elementName);
+        window.location.href = currentUrl.toString();
+      });
+    }
+  });
+}
